63b8dd5f52195d429cf401c062643520c201b766
Adam Letenyei <letenyeidam89@gmail.com>
2020-01-18 18:59:04 +0100

    Lottery six new appereance

--- src/Controllers/MainController.java
+++ src/Controllers/MainController.java
@@ -4,23 +4,37 @@ import DesignLoaders.GiveNewWindow;
 import DesignLoaders.NewWindow;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+
+import java.io.IOException;
+
 
 public class MainController {
 
     @FXML
-    Button SixLottery_bt, SevenLottery_bt,FiveLottery_bt;
+    Button SixLottery_bt, SevenLottery_bt, FiveLottery_bt;
+    @FXML
+    AnchorPane split_left_ahp;
 
-    public void sixOnAction(ActionEvent e){
-        NewWindow sixLotteryWindow = new GiveNewWindow("DesignFiles/SixStyle.fxml","Six lottery");
-        sixLotteryWindow.createNewWindow();
+    public void sixOnAction(ActionEvent e) throws IOException {
+
+        //NewWindow sixLotteryWindow = new GiveNewWindow("DesignFiles/SixStyle.fxml", "Six lottery");
+        split_left_ahp.getChildren().add(FXMLLoader.load(getClass().getClassLoader().getResource("DesignFiles/SixStyle.fxml")));
+        split_left_ahp.setPrefHeight(399);
+        split_left_ahp.setPrefWidth(600);
+        //sixLotteryWindow.createNewWindow();
     }
-    public void sevenOnAction(ActionEvent e){
-        NewWindow sevenLotteryWindow = new GiveNewWindow("DesignFiles/SevenStyle.fxml","Scandinavian lottery");
+
+    public void sevenOnAction(ActionEvent e) {
+        NewWindow sevenLotteryWindow = new GiveNewWindow("DesignFiles/SevenStyle.fxml", "Scandinavian lottery");
         sevenLotteryWindow.createNewWindow();
     }
-    public void fiveOnAction(ActionEvent e){
-        NewWindow fiveLotteryWindow = new GiveNewWindow("DesignFiles/FiveStyle.fxml","Five lottery");
+
+    public void fiveOnAction(ActionEvent e) {
+        NewWindow fiveLotteryWindow = new GiveNewWindow("DesignFiles/FiveStyle.fxml", "Five lottery");
         fiveLotteryWindow.createNewWindow();
     }
 
